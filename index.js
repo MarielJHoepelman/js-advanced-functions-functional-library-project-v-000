c2442b232ed495f471142140b31b7791d9026409
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-21 13:27:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ const fi = (function() {
     sortBy: function(collection, callback) {
       // let result = this.map(collection, callback)
       //  console.log(collection, result)
-      let result = collection;
+      const result = collection;
       return collection.sort((a, b) => callback(a) - callback(b));
     },
 
