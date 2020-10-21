c7dcd70d59a1c82299987afa5c8124fd10149f67
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-21 14:53:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ const fi = (function() {
 
     flatten: function(collection, isSingleLevel) {
       let result = [];
-
+  
       for (let element of collection){
         if (typeof element === "object") {
             result.push(...this.flatten(element, false))
