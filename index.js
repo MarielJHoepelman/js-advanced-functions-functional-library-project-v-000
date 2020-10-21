cde7321deb65cd0a91c7becda5ad17e9f5466966
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-21 18:39:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -128,8 +128,8 @@ const fi = (function() {
 
           for(let element of collection) {
             let result = iteratee(element)
-            console.log(modulos.find(x => x === result))
-            if(!modulos.find(x => x === result)){
+            console.log(!!!modulos.find(x => x === result))
+            if(!!!modulos.find(x => x === result)){
               modulos.push(result)
               solution.push(element)
             }
